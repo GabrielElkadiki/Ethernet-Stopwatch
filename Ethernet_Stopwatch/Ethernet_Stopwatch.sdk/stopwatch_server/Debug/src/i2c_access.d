c6bed232dd33fc807aa515f879e72b22ca1c49b5
GabrielElkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2019-05-23 09:42:51 -0400

    Streamlined project for version control
    Converted vivado projects to tcl builds for Block Diagram and project modules

--- Ethernet_Stopwatch/Ethernet_Stopwatch.sdk/stopwatch_server/Debug/src/i2c_access.d
+++
@@ -1,4 +0,0 @@
-src/i2c_access.o src/i2c_access.o: ../src/i2c_access.c \
- ../../stopwatch_server_bsp/microblaze_0/include/xparameters.h
-
-../../stopwatch_server_bsp/microblaze_0/include/xparameters.h:
--- Ethernet_Stopwatch/Ethernet_Stopwatch.sdk/stopwatch_server/Debug/src/sfp.d
+++
@@ -1,4 +0,0 @@
-src/sfp.o src/sfp.o: ../src/sfp.c \
- ../../stopwatch_server_bsp/microblaze_0/include/xparameters.h
-
-../../stopwatch_server_bsp/microblaze_0/include/xparameters.h:
