fa2072430ad6c198f3b8d23f9632addbe7ab84bf
GabrielElkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2019-07-07 22:11:24 -0400

    Removed C debug

--- Ethernet_Stopwatch/sdk/stopwatch_server/Debug/src/iic_phyreset.d
+++
@@ -1,4 +0,0 @@
-src/iic_phyreset.o src/iic_phyreset.o: ../src/iic_phyreset.c \
- ../../stopwatch_server_bsp/microblaze_0/include/xparameters.h
-
-../../stopwatch_server_bsp/microblaze_0/include/xparameters.h:
--- Ethernet_Stopwatch/sdk/stopwatch_server/Debug/src/si5324.d
+++
@@ -1,4 +0,0 @@
-src/si5324.o src/si5324.o: ../src/si5324.c \
- ../../stopwatch_server_bsp/microblaze_0/include/xparameters.h
-
-../../stopwatch_server_bsp/microblaze_0/include/xparameters.h:
