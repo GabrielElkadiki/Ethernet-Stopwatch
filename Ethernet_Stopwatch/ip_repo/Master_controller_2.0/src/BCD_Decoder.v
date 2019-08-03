38f1b229a603728910eb01a78d12f8fc8abdf2b2
Gabriel Elkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2019-08-02 23:14:48 -0400

    Update BCD_Decoder.v

--- Ethernet_Stopwatch/ip_repo/Master_controller_2.0/src/BCD_Decoder.v
+++ Ethernet_Stopwatch/ip_repo/Master_controller_2.0/src/BCD_Decoder.v
@@ -22,7 +22,7 @@
 
 module BCD_Decoder(
     input [3:0]v,
-	input [2:0] anum,
+    input [2:0] anum,
     output [6:0]seg,
     output [7:0]an
     );
@@ -36,22 +36,18 @@ module BCD_Decoder(
         .D      (v[3]),
         .z      (z)
     );
-    
     Circuit_A circuitA(
         .A      (v[0]),
         .B      (v[1]),
         .C      (v[2]),
         .m      (circAin)
     );
-    
-    
     twoToOneMUX mux0(
         .y      (v[0]),
         .x      (circAin[0]),
         .s      (z),
         .m      (m[0])
     );
-    
     twoToOneMUX mux1(
         .y      (v[1]),
         .x      (circAin[1]),
@@ -77,6 +73,7 @@ module BCD_Decoder(
         .an     (an) 
     );
 endmodule
+
 
 module Comparator(
     input A,
@@ -116,34 +113,34 @@ endmodule
 
 module segment_anode_set(
     input [3:0]bcd,
-	input [2:0] anum,
+    input [2:0] anum,
     output reg [6:0]seg,
     output reg [7:0]an
     );
     always @(bcd or anum) begin
-		 case(anum)
-			0: an = 8'b11111110;
-			1: an = 8'b11111101;
-			2: an = 8'b11111011;
-			3: an = 8'b11110111;
-			4: an = 8'b11101111;
-			5: an = 8'b11011111;
-			6: an = 8'b10111111;
-			7: an = 8'b01111111;
-			default: an = 8'b11111111;
-		 endcase
-		 case (bcd) 
-			0 : seg = 7'b1000000;
-			1 : seg = 7'b1111001;
-			2 : seg = 7'b0100100;
-			3 : seg = 7'b0110000;
-			4 : seg = 7'b0011001;
-			5 : seg = 7'b0010010;
-			6 : seg = 7'b0000010;
-			7 : seg = 7'b1111000;
-			8 : seg = 7'b0000000;
-			9 : seg = 7'b0010000;
-			default : seg = 7'b0000000; 
-		 endcase
+	 case(anum)
+		0: an = 8'b11111110;
+		1: an = 8'b11111101;
+		2: an = 8'b11111011;
+		3: an = 8'b11110111;
+		4: an = 8'b11101111;
+		5: an = 8'b11011111;
+		6: an = 8'b10111111;
+		7: an = 8'b01111111;
+		default: an = 8'b11111111;
+	endcase
+	 case (bcd) 
+		0 : seg = 7'b1000000;
+		1 : seg = 7'b1111001;
+		2 : seg = 7'b0100100;
+		3 : seg = 7'b0110000;
+		4 : seg = 7'b0011001;
+		5 : seg = 7'b0010010;
+		6 : seg = 7'b0000010;
+		7 : seg = 7'b1111000;
+		8 : seg = 7'b0000000;
+		9 : seg = 7'b0010000;
+		default : seg = 7'b0000000; 
+	 endcase
 	end
 endmodule
