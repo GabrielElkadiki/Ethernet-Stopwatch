3b36b1e96f20d30020868ac95e2210207c0609dd
Gabriel Elkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2020-01-10 19:11:31 -0500

    Removed legacy code from BCD decoder

--- Ethernet_Stopwatch/ip_repo/Master_controller_2.0/src/BCD_Decoder.v
+++ Ethernet_Stopwatch/ip_repo/Master_controller_2.0/src/BCD_Decoder.v
@@ -20,104 +20,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module BCD_Decoder(
-    input [3:0]v,
-    input [2:0] anum,
-    output [6:0]seg,
-    output [7:0]an
-    );
-    wire [2:0]circAin;
-    wire [3:0]m;
-    wire z;
-    Comparator comperator(
-        .A      (v[0]),
-        .B      (v[1]),
-        .C      (v[2]), 
-        .D      (v[3]),
-        .z      (z)
-    );
-    Circuit_A circuitA(
-        .A      (v[0]),
-        .B      (v[1]),
-        .C      (v[2]),
-        .m      (circAin)
-    );
-    twoToOneMUX mux0(
-        .y      (v[0]),
-        .x      (circAin[0]),
-        .s      (z),
-        .m      (m[0])
-    );
-    twoToOneMUX mux1(
-        .y      (v[1]),
-        .x      (circAin[1]),
-        .s      (z),
-        .m      (m[1])
-    );
-    twoToOneMUX mux2(
-        .y      (v[2]),
-        .x      (circAin[2]),
-        .s      (z),
-        .m      (m[2])
-    );    
-    twoToOneMUX mux3(
-        .y      (v[3]),
-        .x      (1'b0),
-        .s      (z),
-        .m      (m[3])
-    );
-    segment_anode_set seg_an(
-		.anum	(anum),
-        .bcd    (m),
-        .seg    (seg),
-        .an     (an) 
-    );
-endmodule
-
-
-module Comparator(
-    input A,
-    input B,
-    input C,
-    input D,
-    output z
-    );
-    assign z = D & ((!A & B & !C) | (A & B & !C) | (!A & !B &C) | (A & !B & C) | (!A & B & C) | (A & B & C));
-endmodule
-
-
-module Circuit_A(
-    input A,
-    input B,
-    input C,
-    output [2:0]m
-    );
-    assign m[0] = (!C & B & A) | (C & !B & A) | (C & B & A);
-    assign m[1] = (C & !B & !A) | (C & !B & A);
-    assign m[2] = (C & B & !A) | (C & B & A);
-endmodule
-
-
-module twoToOneMUX(
-    input x, y,
-    input s,
-    output reg m
-    );
-    always @(x or y or s)
-        if(s == 1)
-           m = x;
-        else
-           m = y;
-endmodule
-
-
-module segment_anode_set(
-    input [3:0]bcd,
+module bcd_decoder(
+    input [3:0] v,
     input [2:0] anum,
     output reg [6:0]seg,
     output reg [7:0]an
     );
-    always @(bcd or anum) begin
+    always @(v or anum) begin
 	 case(anum)
 		0: an = 8'b11111110;
 		1: an = 8'b11111101;
@@ -128,8 +37,8 @@ module segment_anode_set(
 		6: an = 8'b10111111;
 		7: an = 8'b01111111;
 		default: an = 8'b11111111;
-	endcase
-	 case (bcd) 
+	 endcase
+	 case (v) 
 		0 : seg = 7'b1000000;
 		1 : seg = 7'b1111001;
 		2 : seg = 7'b0100100;
